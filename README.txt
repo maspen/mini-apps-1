9a0b01918df6919bde9e476635389e0e3354401e
Matt Aspen <mattaspen@gmail.com>
2018-05-21 17:00:55 -0700

    basic workings

---
+++ README.txt
@@ -0,0 +1,25 @@
+install nvm
+https://github.com/creationix/nvm
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 6.13.1
+nvm alias default 6.13.1
+
+*** run in each folder:
+npm init --> creates package.json
+
+npm install --save express
+
+npm install --save-dev nodemon
+
+* notes:
+
+// body parser - removes req.end ...
+
+app.get('/users', function(req, res, next) {
+	// list users
+})
--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,21 +1,83 @@
-var express = require('express');
-var http  = require('http');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.use(express.static('./public'))
-// var path = require('path');
-// app.use(express.static(path.join(__dirname, 'public')));
+// create model for data structure
+//  array of arrays
+// listen for click events on table
 
-const port = 3000;
+/*
+	[[0,1,2],
+	 [3,4,5],
+	 [6,7,8]]
+*/
 
-app.listen(port, function () {
-	console.log('mini apps - challenge_1 running on ', port);
-});
+var state = [ new Array(3),
+						  new Array(3),
+						  new Array(3) ];
 
-// app.use('/', index);
+// var mapping = {
+// 	0: this.state[0][0],
+// 	1: this.state[0][1],
+// 	2: this.state[0][2],
+// 	3: this.state[1][0],
+// 	4: this.state[1][1],
+// 	5: this.state[1][2],
+// 	6: this.state[2][0],
+// 	7: this.state[2][1],
+// 	8: this.state[2][2],
+// };
+var mapping = {
+	0: [0, 0],
+	1: [0, 1],
+	2: [0, 2],
+	3: [1, 0],
+	4: [1, 1],
+	5: [1, 2],
+	6: [2, 0],
+	7: [2, 1],
+	8: [2, 2],
+};
 
-app.get('/', function (req, res) {
-	res.render('../public/index');
-});
+var x = 'X';
+var o = 'O';
+var firstMove = true;
+var previous;
 
-module.exports = app;
+var clickEvent = function(id) {
+	// console.log("clickEvent", id);
+	if (firstMove) {
+		setX(id);
+		firstMove = false;
+		previous = x;
+	} else if (previous === x) {
+		previous = o;
+		setO(id);
+	} else {
+		previous = x;
+		setX(id);
+	}
+}
+
+var setX = function(id) {
+	// TODO: check is this has alreaddy been set
+	var element = document.getElementById(id);
+	element.innerHTML = x;
+
+	setState(id, x);
+}
+
+var setO = function(id) {
+	// TODO: check is this has alreaddy been set
+	var element = document.getElementById(id);
+	element.innerHTML = o;
+
+	setState(id, o);
+}
+
+var setState = function(id, letter) {
+
+	var stateLocationArray = mapping[id];
+	// [0, 1]
+	state[stateLocationArray[0]][stateLocationArray[1]] = letter;
+	// check for winner
+}
