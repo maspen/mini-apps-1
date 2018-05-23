103eead96af0367310c2e350092845c350eb82d9
Matt Aspen <mattaspen@gmail.com>
2018-05-23 08:30:16 -0700

    got json to csv parser to work

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 var bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
+var _ = require('lodash');
 
 app.use(express.static('./client'));
 app.use(bodyParser.json());
@@ -35,17 +36,95 @@ app.post('/csv', function(req, res){
 	res.send(JSON.stringify('Hello World from cvs'));
 });
 
+var getCsv = (jsonFilePath, callback) => {
+	console.log('inside jsonGet');
+
+	fs.readFile(jsonFilePath, (err, data) => {
+	  if (err) {
+	  	callback(err, null);
+	  };
+	  callback(null, JSON.parse(data));
+	});
+};
+
 /** ---- json parsing ----- */
 var getCsvCallback = function(error, data) {
 	if (error) {
 		console.log('getCsvCallback error', error);
 		return;
 	}
-	//console.log('getCsvCallback data', data);
 
-	// change jsonFilePath tp retulData
-	//jsonGet(jsonFilePath);
-	jsonParser(data, parserCallback);
+	createCSVString(data);
+}
+
+var createCSVString = (object) => {
+	// get heading
+	var headingArr = Object.getOwnPropertyNames(object);
+/*
+[ 'firstName',
+  'lastName',
+  'county',
+  'city',
+  'role',
+  'sales',
+  'children' ]
+*/
+	// need to trim 'children'
+	headingArr = _.dropRight(headingArr);
+/*
+[ 'firstName', 'lastName', 'county', 'city', 'role', 'sales' ]
+*/
+	var linesArr = [];
+
+	var recurse = (obj) => {
+
+		_.forEach(obj, function(value, key) {
+			if(_.isArray(value)) {
+				if (_.isEmpty(value)) {
+					// do nothing
+				} else {
+					_.forEach(value, function(value) {
+					recurse(value);
+					})
+				}
+			} else {
+				linesArr.push(value);
+			}
+		})
+	};
+
+	recurse(object);
+
+	// need to create string with line breaks after each
+	// character that is headingArr.length - 1
+	var csv = '';
+	// get the length of heading
+	var lineLength = headingArr.length;
+
+	// concatenate heading & linesArr
+	var allLinesArray = _.concat(headingArr, linesArr);
+	var lineLengthCounter = 0;
+	_.forEach(allLinesArray, function(word){
+		csv += word;
+		csv += ",";
+		lineLengthCounter++;
+		if(lineLengthCounter === lineLength) {
+			// trim the last ','
+			csv = csv.substring(0, csv.length - 1);
+			csv += '\n';
+			lineLengthCounter = 0;
+		}
+	});
+	
+	console.log(csv);
+/*
+firstName,lastName,county,city,role,sales
+Joshie,Wyattson,San Mateo,San Mateo,Broker,1000000
+Beth Jr.,Johnson,San Mateo,Pacifica,Manager,2900000
+Smitty,Won,San Mateo,Redwood City,Sales Person,4800000
+Allen,Price,San Mateo,Burlingame,Sales Person,2500000
+Beth,Johnson,San Francisco,San Francisco,Broker/Sales Person,7500000
+*/	
 }
 
 var parserCallback = (error, data) => {
@@ -54,100 +133,7 @@ var parserCallback = (error, data) => {
 		return;
 	}
 	console.log('parserCallback data', data);
-}
-
-var getCsv = (jsonFilePath, callback) => {
-	console.log('inside jsonGet');
-
-	fs.readFile(jsonFilePath, (err, data) => {
-	  if (err) {
-	  	callback(err, null);
-	  };
-	  callback(null, JSON.parse(data)); // partial doc
-/*
-getCsvCallback data { firstName: 'Joshie',
-  lastName: 'Wyattson',
-  county: 'San Mateo',
-  city: 'San Mateo',
-  role: 'Broker',
-  sales: 1000000,
-  children: 
-   [ { firstName: 'Beth Jr.',
-       lastName: 'Johnson',
-       county: 'San Mateo',
-       city: 'Pacifica',
-       role: 'Manager',
-       sales: 2900000,
-       children: [Object] },
-     { firstName: 'Beth',
-       lastName: 'Johnson',
-       county: 'San Francisco',
-       city: 'San Francisco',
-       role: 'Broker/Sales Person',
-       sales: 7500000,
-       children: [] } ] }
-*/	  
-	  // callback(null, JSON.stringify(data)); // just #s
-	});
 };
-
-var jsonParser = (JSONData, callback) => {
-	console.log('inside jsonParser');
-	if(null === JSONData || JSONData === '') {
-		callback('file was null or emptu', null);
-	}
-
-	// console.log('JSONData', JSONData);
-
-	// create the heading
-	var heading = ''
-	for (let key in JSONData) {
-		if(key !== "children") {
-			heading += key + ',';
-		}
-	}
-	// trim last comma
-	heading = heading.substring(0, heading.length - 1);
-	heading += '\n';
-
-	console.log('heading:', heading);
-
-	var lines = '';
-	var printObject = (obj, lines, callback) => {
-		for(var key in obj) {
-			if(key !== 'children') {
-				lines += obj[key] + ',';
-			}
-		}
-		// trim last ','
-		lines = lines.substring(0, lines.length - 1);
-		// add new line
-		lines += '\n';
-
-		callback(null, lines);
-	};
-
-	printObject(JSONData, '', callback);
-
-	/*
-	else {
-				// trim last ','
-				lines = lines.substring(0, lines.length - 1);
-				// add new line
-				lines += '\n';
-
-				var childArr = obj.key;
-				if(childArr.length !== 0) {
-					childArr.forEach(function(child){
-						printObject(child);
-					})
-				}
-*/
-
-	// console.log('lines:', lines);
-
-	// callback(null, 'PARSED');
-}
 
 http.createServer(app).listen(3000);
 console.log('Example app listening on port 3000!')
